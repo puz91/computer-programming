188830e938282ea472af3325d2a3368362810d0c
puz91 <151368142+puz91@users.noreply.github.com>
2024-08-13 13:53:20 +0700

    14_majorchange.cpp

--- my com prog/if/14_majorchange.cpp
+++ my com prog/if/14_majorchange.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <string>
+
 using namespace std;
-int main() {
+
+int main()
+
+{
     string nx,ny,max;
     float gpax,gpay;
     char comx,comy,cal1x,cal1y,cal2x,cal2y;
     cin>>nx>>gpax>>comx>>cal1x>>cal2x>>ny>>gpay>>comy>>cal1y>>cal2y;
-    if(comx=='A'&&comy=='A'&&cal1x<='C'&&cal2x<='C'&&cal1y<='C'&&cal2y<='C'){
+    if(comx!='A' || cal1x>'C' || cal2x>'C'){
+        nx="0";
+    }
+    if(comy!='A' || cal1y>'C' || cal2y>'C'){
+        ny="0";
+    }
+    else{
         if(gpax>gpay){
             max=nx;
         }
@@ -35,8 +46,17 @@ int main() {
             }
         }
     }
-    else{
+    //cout<<nx<<" "<<ny<<" ";
+    if(nx=="0"&&ny=="0"){
         cout<<"None";
+        return 0;
+    }
+    else if(nx=="0"){
+        cout<<ny;
+        return 0;
+    }
+    else if(ny=="0"){
+        cout<<nx;
         return 0;
     }
     cout<<max;
